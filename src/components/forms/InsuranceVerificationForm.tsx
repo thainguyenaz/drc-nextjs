1be7c4594f53e1f0e1f00dd94a60248c9878be8d
Thai Nguyen <thai@desertrecoverycenters.com>
2026-06-20 04:16:37 +0000

    Defer Turnstile script to lazyOnload to take it off the homepage critical path

--- src/components/forms/InsuranceVerificationForm.tsx
+++ src/components/forms/InsuranceVerificationForm.tsx
@@ -267,7 +267,7 @@ export default function InsuranceVerificationForm() {
         <Script
           id="cf-turnstile-script"
           src="https://challenges.cloudflare.com/turnstile/v0/api.js"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           async
           defer
         />
